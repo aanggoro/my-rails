4361248c94cc40b75ef16ae40152d78851a8264b
aanggoro <1511501288@student.budiluhur.ac.id>
2017-08-24 23:59:55 +0700

    add detail in user model

--- auth/app/models/user.rb
+++ auth/app/models/user.rb
@@ -1,14 +1,14 @@
 class User < ApplicationRecord
 
-  before_save { self.email = email.downcase }
-  validates :name, presence: true, length: { maximum: 50 }
+  before_save { self.email = email.downcase } # create all email character lower case
+  validates :name, presence: true, length: { maximum: 50 } # validation for name must persence maximum length 50 character
 
-  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+[a-z]+\z/i
-  validates :email, presence: true, length: { maximum: 255 },
-                    format: { with: VALID_EMAIL_REGEX },
-                    uniqueness: { case_sensitive: false }
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+[a-z]+\z/i # key of regex
+  validates :email, presence: true, length: { maximum: 255 }, # validation for email must persence maximum length 255 character
+                    format: { with: VALID_EMAIL_REGEX }, # not allow if not email format
+                    uniqueness: { case_sensitive: false } # unique that not case sensitive
 
-  has_secure_password
+  has_secure_password # all data paramater will encrypt
 
-  validates :password, presence: true, length: { minimum: 6 }
+  validates :password, presence: true, length: { minimum: 6 } #validation for password must persence minimum length 6 character
 end
