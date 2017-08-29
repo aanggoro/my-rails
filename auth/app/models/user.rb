822e46eed88d09d718b2adf87b15de05af257bc9
aanggoro <1511501288@student.budiluhur.ac.id>
2017-08-29 08:12:42 +0700

    add validate password

--- auth/app/models/user.rb
+++ auth/app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
 
-  before_save { self.email = email.downcase! }
+  before_save { email.downcase! }
   validates :name,  presence: true, length: { maximum: 50 }
 
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
@@ -8,5 +8,7 @@ class User < ApplicationRecord
                     format: { with: VALID_EMAIL_REGEX },
                     uniqueness: { case_sensitive: false }
 
-  has_secure_password                  
+  has_secure_password
+
+  validates :password, presence: true, length: { minimum: 6 }
 end
