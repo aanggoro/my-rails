7b3281583b9601761734d440b0a3cd8accf1e627
aanggoro <1511501288@student.budiluhur.ac.id>
2017-09-01 21:13:25 +0700

    add login

--- new_auth/config/routes.rb
+++ new_auth/config/routes.rb
@@ -6,5 +6,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   get 'signup', to: 'users#new'
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  post 'logout', to: 'sessions#destroy'
   resources :users
 end
