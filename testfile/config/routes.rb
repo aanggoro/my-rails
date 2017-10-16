6b541960633fe797d25e50e30627aeb20118a780
aanggoro <1511501288@student.budiluhur.ac.id>
2017-10-16 11:42:54 +0700

    add routes

--- testfile/config/routes.rb
+++ testfile/config/routes.rb
@@ -1,11 +1,6 @@
 Rails.application.routes.draw do
-  get 'resume/index'
-
-  get 'resume/new'
-
-  get 'resume/create'
-
-  get 'resume/destroy'
+  resources :resume, only: [:index, :new, :create, :destroy]
+  root 'resume#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
