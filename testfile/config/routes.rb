62a01cc97de003a2e84ea72668a8ba5c82116b98
aanggoro <1511501288@student.budiluhur.ac.id>
2017-10-16 11:40:16 +0700

    add migration attacment, name and validation

--- testfile/config/routes.rb
+++ testfile/config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+  get 'resume/index'
+
+  get 'resume/new'
+
+  get 'resume/create'
+
+  get 'resume/destroy'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
---
+++ testfile/test/controllers/resume_controller_test.rb
@@ -0,0 +1,24 @@
+require 'test_helper'
+
+class ResumeControllerTest < ActionDispatch::IntegrationTest
+  test "should get index" do
+    get resume_index_url
+    assert_response :success
+  end
+
+  test "should get new" do
+    get resume_new_url
+    assert_response :success
+  end
+
+  test "should get create" do
+    get resume_create_url
+    assert_response :success
+  end
+
+  test "should get destroy" do
+    get resume_destroy_url
+    assert_response :success
+  end
+
+end
